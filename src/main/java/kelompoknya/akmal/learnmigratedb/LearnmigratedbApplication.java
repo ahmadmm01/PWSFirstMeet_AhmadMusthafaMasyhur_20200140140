9fc00969acace7d5945ff24c43a677080f7c7308
MADD <MADD@DESKTOP-3CQCFHH>
2022-10-16 20:30:40 +0700

    first commit

---
+++ src/main/java/kelompoknya/akmal/learnmigratedb/LearnmigratedbApplication.java
@@ -0,0 +1,13 @@
+package kelompoknya.akmal.learnmigratedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnmigratedbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnmigratedbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kelompoknya/akmal/learnmigratedb/LearnmigratedbApplicationTests.java
@@ -0,0 +1,13 @@
+package kelompoknya.akmal.learnmigratedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnmigratedbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
